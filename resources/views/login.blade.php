5e33b0a4126c68d5f1f53e61f4afe373dc79753e
Edgar56 <edgaras.chalilovas@gmail.com>
2018-06-22 17:50:24 +0300

    Initial commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    {{--{!! $map['js'] !!}--}}
+</head>
+<body>
+<center>
+ <form action="/loginme" method="post">
+
+        <input type="hidden" name="_toked" value="{{csrf_token()}}}">
+         USERNAME:<input type="text" name="username">
+         PASSWORD:<input type="password" name="password">
+         <input type="submit" name="login" value="Login">
+
+
+ </form>
+</center>
+{{--{!! $map['html'] !!}--}}
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+//    $config['center'] = 'Vilnius Lithuania';
+//    $config['zoom'] = '14';
+//    $config['map_height'] = '500px';
+//    $config['scrollwheel'] = false;
+//
+//    GMaps::initialize($config);
+//
+//    $map = GMaps::create_map();
+
+    return view('welcome');//->with('map', $map);
+
+});
+//Route::post('/loginme',function (){
+// echo "work";
+//});
+
+Route::resource('data','DataController');
